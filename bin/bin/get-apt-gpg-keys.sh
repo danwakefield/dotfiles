f941ef59d9c45e561c796e9ebb23fda7ddf52150
Daniel Wakefield <daniel.wakefield@hp.com>
2015-05-21 16:12:37 +0100

    Duplicate file moved to the correct place.

--- bin/bin/get-apt-gpg-keys.sh
+++ bin/bin/get-apt-gpg-keys.sh
@@ -2,6 +2,8 @@
 #
 # Fix GPG key errors.
 
+# order of redirection is not a mistake
+# we want to keep stderr and dump stdout
 sudo apt-get update 2>&1 1>/dev/null \
     | grep "W: GPG" \
     | cut -d: -f6 \
