f09cacbf6f0f4dc51dfba62e08b0f3b5f4eff7a2
Daniel <admx@danielwakefield.info>
2015-05-21 15:23:47 +0000

    Add ttrss_dump
    Signed-off-by: Daniel Wakefield <daniel.wakefield@hp.com>

---
+++ bin/bin/ttrss_dump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DUMP_NAME="ttrss_$(date -Ihours | sed 's/+.*//').gz"
+DUMP_DIR="$HOME/backup/db"
+
+[[ -d $DUMP_DIR ]] || mkdir -p $DUMP_DIR
+
+pg_dump -U ttrss ttrss | bzip2 > "$DUMP_DIR/$DUMP_NAME"
+
