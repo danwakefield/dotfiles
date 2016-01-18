1a680478800345dd4e76cf13eb70ff6a0d8df2f2
Daniel Wakefield <dwakefieldmail-git@yahoo.co.uk>
2016-01-18 16:59:54 +0000

    Change Gzip to Pigz where available

---
+++ bin/bin/reduce-video-quality.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -z $1 ]; then
+    echo "Enter the filename to convert"
+    exit
+fi
+avconv -i $1 -sws_flags lanczos+accurate_rnd -vf "scale=960:540" -c:v libx264 -crf 20 -preset veryslow -profile:v main -tune fastdecode -c:a copy ${1}.downsampled.mkv
--- zsh/.config/zsh_custom/gzip-directory.sh
+++ zsh/.config/zsh_custom/gzip-directory.sh
@@ -5,7 +5,13 @@ function gzipd {
 		echo "gzipd requires a folder/file to compress"
 		return 1
 	fi
+	gzipd_output="$(basename $1)-$(date -I).tgz"
 
-	gzipd_output="$(basename $1).tgz"
-	tar -czvf "$gzipd_output" "$1"
+	which pigz > /dev/null
+	if [[ $? == 0 ]]; then
+		tar -c "$1" | pigz -c > "$gzipd_output"
+	else
+		echo "Using tar, Install pigz to speed up gzipd"
+		tar -czvf "$gzipd_output" "$1"
+	fi
 }
