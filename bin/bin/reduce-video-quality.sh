99d7c065d65d57fa94d87b126b419adc0099084d
Daniel Wakefield <dwakefieldmail-git@yahoo.co.uk>
2016-02-17 19:01:07 +0000

    Create journal for dissertation.

--- bin/bin/reduce-video-quality.sh
+++ bin/bin/reduce-video-quality.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "Enter the filename to convert"
     exit
 fi
-avconv -i $1 -sws_flags lanczos+accurate_rnd -vf "scale=960:540" -c:v libx264 -crf 20 -preset veryslow -profile:v main -tune fastdecode -c:a copy ${1}.downsampled.mkv
+time avconv -i "$1" -sws_flags lanczos+accurate_rnd -vf "scale=960:540" -c:v libx264 -crf 20 -preset veryslow -profile:v main -tune fastdecode -c:a copy ${1}.downsampled.mkv
