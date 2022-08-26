4c9234026df78779e865f6962ee89aa3c229ccda
Daniel Wakefield <dan@danw.xyz>
2022-08-26 21:27:31 +0100

    Update

--- zsh/.config/zsh_custom/homestead.sh
+++
@@ -1,3 +0,0 @@
-function homestead() {
-    ( cd ~/Homestead && vagrant $* )
-}
--- zsh/.config/zsh_custom/pbpt.pw.sh
+++
@@ -1,9 +0,0 @@
-# https://ptpb.pw
-# https://github.com/ptpb/pb
-pb () {
-    curl -F "c=@${1:--}" https://ptpb.pw/
-}
-
-pbc () {
-    clipit -c | pb
-}
